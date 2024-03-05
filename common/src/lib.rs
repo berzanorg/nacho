b667f1968d619525aedb50107c2cfb9358d486fe
Berzan <berzan@tutanota.com>
2024-03-05 17:28:33 +0000

    impl: field element

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod field;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use field::{field_from_bytes, field_to_bytes, Field};
